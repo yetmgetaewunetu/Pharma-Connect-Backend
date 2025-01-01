4ba33ae9f343f3e0613d06b7da7f0304411bc2b2
Yetmgeta Ewunetu <yetmgetae@gmail.com>
2025-01-01 23:56:28 +0300

    updating serverjs

--- controller/PharmacyController.js
+++ controller/PharmacyController.js
@@ -1,62 +1,57 @@
 const pharmacyService = require("../services/pharamcyServices");
-const asyncErrorHandler = require('../utils/asyncErrorHandler')
-
- 
+const asyncErrorHandler = require("../utils/asyncErrorHandler");
 
 // get pharmacy
-exports.getPharmacyController = asyncErrorHandler( async(req, res) => {
-    const pharmacyId = req.params.pharmacyId;
-    const pharmacy = await pharmacyService.getPharmacyById(pharmacyId);
-    res.status(200).json({
-      success: true,
-      data: pharmacy,
-    });
-})
+exports.getPharmacyController = asyncErrorHandler(async (req, res) => {
+  const pharmacyId = req.params.id;
+
+  const pharmacy = await pharmacyService.getPharmacyById(pharmacyId);
+  res.status(200).json({
+    success: true,
+    data: pharmacy,
+  });
+});
 
 // update pharmacy
-exports.updatePharmacyController = asyncErrorHandler( async (req, res) => {
-      const pharmacyId = req.params.id;
-      const updateData = req.body;
-      const updatedPharmacy = await pharmacyService.updatePharmacy(
-        pharmacyId,
-        updateData
-      );
-      res.status(200).json({
-        success: true,
-        data: updatedPharmacy,
-      });
-})
+exports.updatePharmacyController = asyncErrorHandler(async (req, res) => {
+  const pharmacyId = req.params.id;
+  const updateData = req.body;
+  const updatedPharmacy = await pharmacyService.updatePharmacy(
+    pharmacyId,
+    updateData
+  );
+  res.status(200).json({
+    success: true,
+    data: updatedPharmacy,
+  });
+});
 
 // delete pharmacy
-exports.deletePharmacyController = asyncErrorHandler( async (req, res) => { 
-    const pharmacyId = req.params.id;
-    const deletedPharmacy = await pharmacyService.deletePharmacy(pharmacyId);
-    res.status(200).json({
-      success: true,
-      message: "Pharmacy deleted successfully.",
-      data: deletedPharmacy,
-    });
-      
-})
+exports.deletePharmacyController = asyncErrorHandler(async (req, res) => {
+  const pharmacyId = req.params.id;
+  const deletedPharmacy = await pharmacyService.deletePharmacy(pharmacyId);
+  res.status(200).json({
+    success: true,
+    message: "Pharmacy deleted successfully.",
+    data: deletedPharmacy,
+  });
+});
 
 // get pharmacies
-exports.getPharmaciesController = asyncErrorHandler( async (req, res) => {
-      const filter = req.query;
-      const pharmacies = await pharmacyService.getPharmacies(filter);
-      res.status(200).json({
-        success: true,
-        data: pharmacies,
-      });
-      
-})
- 
- 
+exports.getPharmaciesController = asyncErrorHandler(async (req, res) => {
+  const filter = req.query;
+  const pharmacies = await pharmacyService.getPharmacies(filter);
+  res.status(200).json({
+    success: true,
+    data: pharmacies,
+  });
+});
 
 /**manage inventory conrollers */
 
-// get inventory of a pharmacy 
+// get inventory of a pharmacy
 exports.getInventory = asyncErrorHandler(async (req, res) => {
-  const {pharmacyId} = req.params;
+  const { pharmacyId } = req.params;
   const inventory = await pharmacyService.getInventory(pharmacyId);
   res.status(200).json({
     success: true,
@@ -64,24 +59,29 @@ exports.getInventory = asyncErrorHandler(async (req, res) => {
   });
 });
 
-
-// add medicine to inventory  
+// add medicine to inventory
 exports.addInventoryItem = asyncErrorHandler(async (req, res) => {
-  const {pharmacyId} = req.params;
+  const { pharmacyId } = req.params;
   const medicineData = req.body;
-  const inventory = await pharmacyService.addInventoryItem(pharmacyId, medicineData);
+  const inventory = await pharmacyService.addInventoryItem(
+    pharmacyId,
+    medicineData
+  );
   res.status(201).json({
     success: true,
     data: inventory,
   });
 });
 
-
 // update quantity,price & expiredate
 exports.updateInventoryItem = asyncErrorHandler(async (req, res) => {
   const medicineData = req.body;
   const { pharmacyId, medicineId } = req.params;
-  const inventory = await pharmacyService.updateInventoryItem(pharmacyId, medicineId, medicineData);
+  const inventory = await pharmacyService.updateInventoryItem(
+    pharmacyId,
+    medicineId,
+    medicineData
+  );
   res.status(200).json({
     success: true,
     data: inventory,
@@ -91,8 +91,11 @@ exports.updateInventoryItem = asyncErrorHandler(async (req, res) => {
 // delete medicine from inventory
 exports.deleteInventoryItem = asyncErrorHandler(async (req, res) => {
   const { pharmacyId, medicineId } = req.params;
-  
-  const inventory = await pharmacyService.deleteInventoryItem(pharmacyId, medicineId);
+
+  const inventory = await pharmacyService.deleteInventoryItem(
+    pharmacyId,
+    medicineId
+  );
   res.status(200).json({
     success: true,
     message: "Pharmacy deleted successfully.",
--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const connectDB = require('./config/db')
-const CustomError = require('./utils/customError')
-const globalErrorHandler = require('./controller/errorController')
-const app = express()
- 
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
+const CustomError = require("./utils/customError");
+const globalErrorHandler = require("./controller/errorController");
+const app = express();
 
 // Middlewares
 app.use(cors());
@@ -14,7 +12,6 @@ app.use(express.json());
 
 //DB Connect
 connectDB();
-
 
 /*Routes*/
 
@@ -33,39 +30,35 @@ app.use("/api/v1/applications", require("./routes/applicationRoutes"));
 //search routes
 app.use("/api/v1/", require("./routes/searchRoutes"));
 
-
 // Testing
-app.get('/', (req, res) => {
-    res.send("hello world")
-})
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 //Handle not Found
-app.all('*', (req, res, next) => {
-    next(new CustomError(`Can't find ${req.originalUrl} on the server.`,404))
-})
+app.all("*", (req, res, next) => {
+  next(new CustomError(`Can't find ${req.originalUrl} on the server.`, 404));
+});
 
 // Global error handling middleware
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 const server = app.listen(process.env.PORT, () => {
-    console.log("Server is listesning on port:", process.env.PORT)
-})
+  console.log("Server is listesning on port:", process.env.PORT);
+});
 
-// Handle All unhandled promise rejections  
-process.on('unhandledRejection', (error) => {
-    console.log(error.name, error.message);
-    console.log("unhandled rejection occured! shutting down...")
-    server.close(() => {
-        process.exit(1);
-    });
-})
-
-// Handle All uncought exception 
-process.on('uncaughtException', (error) => {
-    console.log(error.name, error.message);
-    console.log("unhandled rejection occured! shutting down...")
+// Handle All unhandled promise rejections
+process.on("unhandledRejection", (error) => {
+  console.log(error.name, error.message);
+  console.log("unhandled rejection occured! shutting down...");
+  server.close(() => {
     process.exit(1);
-})
+  });
+});
 
- 
- 
+// Handle All uncought exception
+process.on("uncaughtException", (error) => {
+  console.log(error.name, error.message);
+  console.log("unhandled rejection occured! shutting down...");
+  process.exit(1);
+});
